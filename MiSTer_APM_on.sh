17bebf199ca4c1712a60e9af51b91ca1c8cb89dc
How@security <h2s@how2security.com.br>
2021-11-18 15:49:23 -0300

    Update MiSTer_APM_on.sh

--- MiSTer_APM_on.sh
+++ MiSTer_APM_on.sh
@@ -63,7 +63,7 @@ function apmHelp()
 	echo -e " MiSTer_APM_Update - Install and Update APM"
 	echo -e " MiSTer_APM_on.sh - Starting play music in /media/fat/Music folder"
 	echo -e " MiSTer_APM_off.sh - Stopping play music"
-	echo -e " MiSTer_APM_Unistall.sh - Unistall APM"
+	echo -e " MiSTer_APM_Uninstall.sh - Uninstall APM"
 	echo -e ""
 	echo -e "Another scripts futire use"
 	exit 2
